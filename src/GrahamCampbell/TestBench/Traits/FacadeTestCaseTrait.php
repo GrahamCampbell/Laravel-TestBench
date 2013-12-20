5bf61fd597352a1f3c05411b92f3f8ad0ff76a5f
Graham Campbell <graham@mineuk.com>
2013-12-20 17:21:54 +0000

    Fix doc block

--- src/GrahamCampbell/TestBench/Traits/FacadeTestCaseTrait.php
+++ src/GrahamCampbell/TestBench/Traits/FacadeTestCaseTrait.php
@@ -19,7 +19,7 @@ namespace GrahamCampbell\TestBench\Traits;
 use ReflectionClass;
 
 /**
- * This is the abstract test case class.
+ * This is the facade test case trait.
  *
  * @package    Laravel-TestBench
  * @author     Graham Campbell
