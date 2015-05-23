49aee47e303e36cdd8301bf4c912f85879b0dd00
Graham Campbell <graham@cachethq.io>
2015-05-23 15:03:34 +0100

    Updated my email

--- src/AbstractAppTestCase.php
+++ src/AbstractAppTestCase.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel TestBench.
  *
- * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Graham Campbell <graham@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -21,7 +21,7 @@ use RuntimeException;
 /**
  * This is the abstract app test case class.
  *
- * @author Graham Campbell <graham@mineuk.com>
+ * @author Graham Campbell <graham@cachethq.io>
  */
 abstract class AbstractAppTestCase extends TestCase
 {
--- src/Traits/LaravelTestCaseTrait.php
+++ src/Traits/LaravelTestCaseTrait.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel TestBench.
  *
- * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Graham Campbell <graham@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@ use Exception;
 /**
  * This is the laravel test case trait.
  *
- * @author Graham Campbell <graham@mineuk.com>
+ * @author Graham Campbell <graham@cachethq.io>
  */
 trait LaravelTestCaseTrait
 {
