7dad2bc428ad4a2bbd79c193301da59bf7af5988
Graham Campbell <graham@alt-three.com>
2017-06-16 20:31:22 +0100

    Preserve BC

--- src/AbstractTestCase.php
+++ src/AbstractTestCase.php
@@ -13,7 +13,7 @@ namespace GrahamCampbell\TestBench;
 
 use GrahamCampbell\TestBenchCore\HelperTrait;
 use GrahamCampbell\TestBenchCore\MockeryTrait;
-use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_TestCase as TestCase;
 
 /**
  * This is the abstract test case class.
