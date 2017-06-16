0502e101eefcc2b2392033b81db07619498d97a4
Graham Campbell <graham@alt-three.com>
2017-06-16 20:31:02 +0100

    Create AbstractNewTestCase.php

---
+++ src/AbstractNewTestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of Laravel TestBench.
+ *
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GrahamCampbell\TestBench;
+
+use GrahamCampbell\TestBenchCore\HelperTrait;
+use GrahamCampbell\TestBenchCore\MockeryTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This is the abstract test case class.
+ *
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+abstract class AbstractNewTestCase extends TestCase
+{
+    use HelperTrait, MockeryTrait;
+}
